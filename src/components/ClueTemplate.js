1c0fbf8a6f4cb0d60fc97635193b9d3cd02ed0b7
gliu36 <gerryliu36@gmail.com>
2019-10-07 02:30:56 -0400

    working on rng table

--- src/components/ClueTemplate.js
+++ src/components/ClueTemplate.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Grid, Cell, List, ListItem, ListItemContent, ListItemAction, Button, DataTable, TableHeader } from 'react-mdl';
 
-import scroll from '../images/scroll.png'
 import Rewards from './Rewards.js'
 import beginner_casket from '../images/beginner_casket.webp'
 import easy_casket from '../images/easy_casket.webp'
@@ -11,8 +10,40 @@ import elite_casket from '../images/elite_casket.webp'
 import master_casket from '../images/master_casket.webp'
 
 import clue_items from './data/clue_scroll_data.json'
+let LEVEL_ROLLS = {'beginner': [1,3], 'easy': [2,4], 'medium': [3,5], 'hard': [4,6], 'elite': [4,6], 'master': [5,7]}
+
+function generateReward(items) {
+	let probs = []
+	let a = 0
+	for (let k in items) {
+		let d_r = items[k]['drop_rate'].replace(/,/,'')
+		let pattern = /(\d+)\/(\d+)/
+		if (pattern.test(d_r)) {
+			let d = d_r.match(pattern)[0]
+
+			
+			probs.push(eval(d))
+
+			a += eval(d)
+		} else {
+			probs.push(0)
+		}
+	}
+	console.log(a)
+	console.log(probs)
+}
 
 
+function get_casket_rewards(level, items) {
+	const range = LEVEL_ROLLS[level]
+	let n = Math.floor(Math.random() * range[1]) + range[0]
+	let rewards = []
+	for (let i = 0; i < n; i++) {
+		rewards.push(generateReward(items))
+	}
+
+	return rewards
+}
 
 
 class ClueTemplate extends Component {
@@ -21,7 +52,8 @@ class ClueTemplate extends Component {
 		super(props)
 
 		this.state = {
-			loot: [{Item: 'test', Quantity: 1, Price: 1}]
+			loot: [{Item: 'test', Quantity: 1, Price: 1}],
+			image_ids: []
 		}
 	}
 
@@ -61,16 +93,50 @@ class ClueTemplate extends Component {
 	}
 
 	handleCasket = (t) => {
-		const items = clue_items[t.toLowerCase()]
-		console.log(items)
+		t = t.toLowerCase()
+		let items = clue_items[t]
+		this.setState({image_ids: items})
+
+		let rewards = get_casket_rewards(t, items)
+
+
+		// for (let k in items) {
+		// 	console.log(items[k]['drop_rate'])
+		// }
+
+		
+
+		// switch (t) {
+		// 	case 'beginner':
+		// 		console.log(t)
+		// 		break;
+		// 	case 'easy':
+		// 		console.log(t)
+		// 		break;
+		// 	case 'medium':
+
+		// 		break;
+		// 	case 'hard':
+
+		// 		break;
+		// 	case 'elite':
+
+		// 		break;
+		// 	case 'master':
+
+		// 		break;
+			
+		// 	default:
+		// 		console.log('hey u broke it')
+		// }
 	}
 	
 
 	render() {
 
 	const diffs = ['Beginner', 'Easy', 'Medium', 'Hard', 'Elite', 'Master']
-	const loot = this.state.loot
-	console.log(clue_items)
+	const { loot, image_ids} = this.state
+	console.log(image_ids)
     return(
 		<div className="clue-body">
 			<Grid className="clue-grid">
@@ -81,7 +147,7 @@ class ClueTemplate extends Component {
 					<p>placeholder</p>
 					<p>placeholder</p>
 
-					<Rewards />
+					<Rewards image-ids={image_ids}/>
 					{/* <img src={scroll} alt="scroll" style={{height: '500px'}} /> */}
 				</Cell>
 				<Cell shadow={2} col={2}>
